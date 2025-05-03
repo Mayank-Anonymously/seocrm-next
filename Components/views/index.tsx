2deb20ba9cff287e717fa7f1e579f6ab58874948
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-03 18:33:36 +0530

    added changes

--- Components/views/index.tsx
+++ Components/views/index.tsx
@@ -1,61 +1,60 @@
-import React, { useEffect } from "react";
-import Breadcrumb from "@common/Breadcrumb";
-import { Card, Col, Container, Row } from "react-bootstrap";
-import Head from "next/head";
-import Basic from "./Basic";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAllJobs } from "Components/slices/jobs/_alljobs/thunk";
+import React, { useEffect } from 'react';
+import Breadcrumb from '@common/Breadcrumb';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import Head from 'next/head';
+import Basic from './Basic';
+import { useDispatch, useSelector } from 'react-redux';
 
 const BarCharts = () => {
-  // const dispatch: any = useDispatch();
-  // const { jobdata, isLoading } = useSelector((state: any) => ({
-  //   jobdata: state.jobFeeds.jobdata,
-  //   isLoading: state.jobFeeds.isLoading,
-  // }));
-  // useEffect(() => {
-  //   fetchAllJobs(dispatch);
-  // }, []);
-  // if (isLoading || jobdata[0] === undefined) {
-  //   return "Please While We get All jobs Data For you";
-  // }
+	// const dispatch: any = useDispatch();
+	// const { jobdata, isLoading } = useSelector((state: any) => ({
+	//   jobdata: state.jobFeeds.jobdata,
+	//   isLoading: state.jobFeeds.isLoading,
+	// }));
+	// useEffect(() => {
+	//   fetchAllJobs(dispatch);
+	// }, []);
+	// if (isLoading || jobdata[0] === undefined) {
+	//   return "Please While We get All jobs Data For you";
+	// }
 
-  // const active_vms = [
-  //   "AHSA",
-  //   "FieldGlass",
-  //   "Kruse",
-  //   "Medefis5",
-  //   "MedicalSolutions",
-  //   "PeopleFluent",
-  //   "ShiftWise",
-  //   "StafferLink",
-  // ];
+	// const active_vms = [
+	//   "AHSA",
+	//   "FieldGlass",
+	//   "Kruse",
+	//   "Medefis5",
+	//   "MedicalSolutions",
+	//   "PeopleFluent",
+	//   "ShiftWise",
+	//   "StafferLink",
+	// ];
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Bar Charts | Hybrix - Admin & Dashboard Template</title>
-      </Head>
-      <>
-        <Container fluid>
-          <Row>
-            <Col lg={12}>
-              <Card>
-                <Card.Header>
-                  <h4 className="card-title mb-0">Basic Bar Chart</h4>
-                </Card.Header>
-                {/* <Card.Body>
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Bar Charts | Hybrix - Admin & Dashboard Template</title>
+			</Head>
+			<>
+				<Container fluid>
+					<Row>
+						<Col lg={12}>
+							<Card>
+								<Card.Header>
+									<h4 className='card-title mb-0'>Basic Bar Chart</h4>
+								</Card.Header>
+								{/* <Card.Body>
                   <Basic
                     dataColors='["--tb-success"]'
                     active_vms={active_vms}
                   />
                 </Card.Body> */}
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    </React.Fragment>
-  );
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+			</>
+		</React.Fragment>
+	);
 };
 
 export default BarCharts;
