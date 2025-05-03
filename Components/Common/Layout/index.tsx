a3ce712df3aab38f1142e5ecfb6774c85281e901
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-03 18:13:47 +0530

    added changes

--- Components/Common/Layout/index.tsx
+++ Components/Common/Layout/index.tsx
@@ -1,151 +1,130 @@
-import React, { useEffect, useState } from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import TopBar from "./TopBar";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import TopBar from './TopBar';
+import { useRouter } from 'next/router';
 
-//import actions
-import {
-  changeLayout,
-  changeSidebarTheme,
-  changeLayoutMode,
-  changeLayoutWidth,
-  changeLayoutPosition,
-  changeTopbarTheme,
-  changeLeftsidebarSizeType,
-  changeLeftsidebarViewType,
-  changeSidebarImageType,
-} from "../../slices/thunk";
-import { useProfile } from "@common/UserHooks";
-import Router from "next/router";
+import { useProfile } from '@common/UserHooks';
+import Router from 'next/router';
 //redux
-import { useSelector, useDispatch } from "react-redux";
-import Footer from "./Footer";
-import RightSidebar from "@common/RightSidebar";
-import LoadingSpinner from "Components/LoaderSpinner";
+import { useSelector, useDispatch } from 'react-redux';
+import Footer from './Footer';
+import RightSidebar from '@common/RightSidebar';
+import LoadingSpinner from 'Components/LoaderSpinner';
 
 const Layout = ({ children }: any) => {
-  const { userProfile } = useProfile();
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  const [requestedPath, setRequestedPath] = useState<any>("");
+	const { userProfile } = useProfile();
+	const router = useRouter();
+	const [loading, setLoading] = useState(false);
+	const [requestedPath, setRequestedPath] = useState<any>('');
 
-  const redirectLoginFunction = () => {
-    if (typeof window !== "undefined") {
-      // Check if we're on the client-side
-      if (!userProfile) {
-        router.push("/auth/login");
-      }
-    }
-  };
+	const redirectLoginFunction = () => {
+		if (typeof window !== 'undefined') {
+			// Check if we're on the client-side
+			if (!userProfile) {
+				router.push('/auth/login');
+			}
+		}
+	};
 
-  useEffect(() => {
-    redirectLoginFunction();
-    const handleRouteChangeStart = (url: string) => {
-      setLoading(true);
-      setRequestedPath(url);
-    };
+	useEffect(() => {
+		redirectLoginFunction();
+		const handleRouteChangeStart = (url: string) => {
+			setLoading(true);
+			setRequestedPath(url);
+		};
 
-    const handleRouteChangeComplete = () => {
-      setLoading(false);
-    };
+		const handleRouteChangeComplete = () => {
+			setLoading(false);
+		};
 
-    const handleRouteChangeError = () => {
-      setLoading(false);
-    };
+		const handleRouteChangeError = () => {
+			setLoading(false);
+		};
 
-    Router.events.on("routeChangeStart", handleRouteChangeStart);
-    Router.events.on("routeChangeComplete", handleRouteChangeComplete);
-    Router.events.on("routeChangeError", handleRouteChangeError);
+		Router.events.on('routeChangeStart', handleRouteChangeStart);
+		Router.events.on('routeChangeComplete', handleRouteChangeComplete);
+		Router.events.on('routeChangeError', handleRouteChangeError);
 
-    // Cleanup event listeners
-    return () => {
-      Router.events.off("routeChangeStart", handleRouteChangeStart);
-      Router.events.off("routeChangeComplete", handleRouteChangeComplete);
-      Router.events.off("routeChangeError", handleRouteChangeError);
-    };
-  }, []);
+		// Cleanup event listeners
+		return () => {
+			Router.events.off('routeChangeStart', handleRouteChangeStart);
+			Router.events.off('routeChangeComplete', handleRouteChangeComplete);
+			Router.events.off('routeChangeError', handleRouteChangeError);
+		};
+	}, []);
 
-  const dispatch: any = useDispatch();
-  const {
-    layoutType,
-    leftSidebarType,
-    layoutModeType,
-    layoutWidthType,
-    layoutPositionType,
-    topbarThemeType,
-    leftsidbarSizeType,
-    leftSidebarViewType,
-    leftSidebarImageType,
-  } = useSelector((state: any) => ({
-    layoutType: state.Layout.layoutType,
-    leftSidebarType: state.Layout.leftSidebarType,
-    layoutModeType: state.Layout.layoutModeType,
-    layoutWidthType: state.Layout.layoutWidthType,
-    layoutPositionType: state.Layout.layoutPositionType,
-    topbarThemeType: state.Layout.topbarThemeType,
-    leftsidbarSizeType: state.Layout.leftsidbarSizeType,
-    leftSidebarViewType: state.Layout.leftSidebarViewType,
-    leftSidebarImageType: state.Layout.leftSidebarImageType,
-  }));
+	const dispatch: any = useDispatch();
+	const {
+		layoutType,
+		leftSidebarType,
+		layoutModeType,
+		layoutWidthType,
+		layoutPositionType,
+		topbarThemeType,
+		leftsidbarSizeType,
+		leftSidebarViewType,
+		leftSidebarImageType,
+	} = useSelector((state: any) => ({
+		layoutType: state.Layout.layoutType,
+		leftSidebarType: state.Layout.leftSidebarType,
+		layoutModeType: state.Layout.layoutModeType,
+		layoutWidthType: state.Layout.layoutWidthType,
+		layoutPositionType: state.Layout.layoutPositionType,
+		topbarThemeType: state.Layout.topbarThemeType,
+		leftsidbarSizeType: state.Layout.leftsidbarSizeType,
+		leftSidebarViewType: state.Layout.leftSidebarViewType,
+		leftSidebarImageType: state.Layout.leftSidebarImageType,
+	}));
 
-  /*
+	/*
     layout settings
     */
-  useEffect(() => {
-    if (
-      layoutType ||
-      leftSidebarType ||
-      layoutModeType ||
-      layoutWidthType ||
-      layoutPositionType ||
-      topbarThemeType ||
-      leftsidbarSizeType ||
-      leftSidebarViewType ||
-      leftSidebarImageType
-    ) {
-      dispatch(changeLeftsidebarViewType(leftSidebarViewType));
-      dispatch(changeLeftsidebarSizeType(leftsidbarSizeType));
-      dispatch(changeSidebarTheme(leftSidebarType));
-      dispatch(changeLayoutMode(layoutModeType));
-      dispatch(changeLayoutWidth(layoutWidthType));
-      dispatch(changeLayoutPosition(layoutPositionType));
-      dispatch(changeTopbarTheme(topbarThemeType));
-      dispatch(changeLayout(layoutType));
-      dispatch(changeSidebarImageType(leftSidebarImageType));
-    }
-  }, [
-    layoutType,
-    leftSidebarType,
-    layoutModeType,
-    layoutWidthType,
-    layoutPositionType,
-    topbarThemeType,
-    leftsidbarSizeType,
-    leftSidebarViewType,
-    leftSidebarImageType,
-    dispatch,
-  ]);
-  return (
-    <React.Fragment>
-      <>
-        <div>
-          <TopBar />
-          <Header />
-          <Sidebar layoutType={layoutType} />
-          <div className="main-content">
-            {loading ? (
-              <LoadingSpinner requestedPath={requestedPath} />
-            ) : (
-              children
-            )}
-          </div>
-          <Footer />
-          <RightSidebar />
-        </div>
-      </>
-    </React.Fragment>
-  );
+	useEffect(() => {
+		if (
+			layoutType ||
+			leftSidebarType ||
+			layoutModeType ||
+			layoutWidthType ||
+			layoutPositionType ||
+			topbarThemeType ||
+			leftsidbarSizeType ||
+			leftSidebarViewType ||
+			leftSidebarImageType
+		) {
+		}
+	}, [
+		layoutType,
+		leftSidebarType,
+		layoutModeType,
+		layoutWidthType,
+		layoutPositionType,
+		topbarThemeType,
+		leftsidbarSizeType,
+		leftSidebarViewType,
+		leftSidebarImageType,
+		dispatch,
+	]);
+	return (
+		<React.Fragment>
+			<>
+				<div>
+					<TopBar />
+					<Header />
+					<Sidebar layoutType={layoutType} />
+					<div className='main-content'>
+						{loading ? (
+							<LoadingSpinner requestedPath={requestedPath} />
+						) : (
+							children
+						)}
+					</div>
+					<Footer />
+					<RightSidebar />
+				</div>
+			</>
+		</React.Fragment>
+	);
 };
 
 export default Layout;
