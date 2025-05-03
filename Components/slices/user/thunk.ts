064a9f7d5f000e5798edea495a1b80f33e63ca01
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-03 20:21:24 +0530

    addded changes

--- Components/slices/user/thunk.ts
+++ Components/slices/user/thunk.ts
@@ -1,23 +1,12 @@
 import {
-  EDIT_USER,
-  GET_ALL_ROLES,
   GET_ALL_USER,
-  MAIL,
-  MANAGER_BY_ROLE,
-  POST_NEW_USER,
-  POST_RESET_,
   hrms_api_host,
-  job_api_host,
-  redirect,
 } from "Components/helpers/url_helper";
 import axios from "axios";
-import { toast } from "react-toastify";
 import {
-  api_is_roles_fetched,
   api_is_userdata_error,
   api_is_userdata_loading,
   api_is_userdata_success,
-  is_user_manager_success,
 } from "./reducers";
 
 export const fetchAllUser = () => async (dispatch: any) => {
