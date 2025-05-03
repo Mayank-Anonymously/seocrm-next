68bdba4646eef84bfde041a0264f2d1e9fac88f4
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-03 21:43:51 +0530

    adde chnages

--- Components/pages_componets/user-queries/user-queries.tsx
+++ Components/pages_componets/user-queries/user-queries.tsx
@@ -1,29 +1,28 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { Table, Card, Col, FormControl  } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import moment from "moment";
-import Link from "next/link";
-import { GetAllQuery } from "Components/slices/user_vacations/thunk";
-import { DELETE_VACATION_BY_ID, baseURL } from "Components/helpers/url_helper";
-import axios from "axios";
-import Swal from "sweetalert2";
+import React, { useEffect, useMemo, useState } from 'react';
+import { Table, Card, Col, FormControl } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
+import Link from 'next/link';
+import { GetAllQuery } from 'Components/slices/user_vacations/thunk';
+import { DELETE_VACATION_BY_ID, baseURL } from 'Components/helpers/url_helper';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import DataTable from 'react-data-table-component';
 
-
-
 const UserVactions = () => {
-  const dispatch: any = useDispatch();
-  const [search, setSearch] = useState("");
+	const dispatch: any = useDispatch();
+	const [search, setSearch] = useState('');
 
-  const data = useSelector((state: any) => state.uservacations.uservacations || []);
+	const data = useSelector(
+		(state: any) => state.uservacations.uservacations || []
+	);
 
+	useEffect(() => {
+		dispatch(GetAllQuery());
+	}, [dispatch]);
 
-  useEffect(() => {
-    dispatch(GetAllQuery());
-  }, [dispatch]);
-
-  // Columns definition
-  const columns = [
+	// Columns definition
+	const columns = [
 		{
 			name: 'Title',
 			selector: (row: any) => row.title,
@@ -73,32 +72,31 @@ const UserVactions = () => {
 		},
 	];
 
-  // Filter the data based on search input
-  const filteredData =[]
+	// Filter the data based on search input
+	const filteredData: any = [];
 
-  return (
-    <Col xl={12}>
-      <Card>
-        <Card.Header>
-          <h4 className="card-title mb-0">All Users Vactions</h4>
-        </Card.Header>
-        <Card.Body>
-          {/* Search Input */}
-    
-        <DataTable
-							columns={columns}
-							data={filteredData.length === 0 ? data : filteredData}
-							pagination
-							defaultSortFieldId={1}
-							subHeader
-						
-							persistTableHead
-							theme={'default'}
-						/>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
+	return (
+		<Col xl={12}>
+			<Card>
+				<Card.Header>
+					<h4 className='card-title mb-0'>All Users Vactions</h4>
+				</Card.Header>
+				<Card.Body>
+					{/* Search Input */}
+
+					<DataTable
+						columns={columns}
+						data={filteredData.length === 0 ? data : filteredData}
+						pagination
+						defaultSortFieldId={1}
+						subHeader
+						persistTableHead
+						theme={'default'}
+					/>
+				</Card.Body>
+			</Card>
+		</Col>
+	);
 };
 
 export default UserVactions;
--- Components/pages_componets/user/user.tsx
+++ Components/pages_componets/user/user.tsx
@@ -1,32 +1,41 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { Table, Card, Col, FormControl,Button, Modal,Form  , Container } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { GetAllUser } from "Components/slices/user/thunk";
-import axios from "axios";
-import Swal from "sweetalert2";
+import React, { useEffect, useMemo, useState } from 'react';
+import {
+	Table,
+	Card,
+	Col,
+	FormControl,
+	Button,
+	Modal,
+	Form,
+	Container,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { GetAllUser } from 'Components/slices/user/thunk';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import DataTable from 'react-data-table-component';
 
-import moment from "moment";
-import Link from "next/link";
-import { UPDATE_USER_WALLET, baseURL } from "Components/helpers/url_helper";
+import moment from 'moment';
+import Link from 'next/link';
+import { UPDATE_USER_WALLET, baseURL } from 'Components/helpers/url_helper';
 
 const User = () => {
-  const dispatch: any = useDispatch();
-  const [search, setSearch] = useState(""); // State for search input
-  const [userId,setUserId] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [formData, setFormData] = useState({
-    paymentAmount: ''
-  });
+	const dispatch: any = useDispatch();
+	const [search, setSearch] = useState(''); // State for search input
+	const [userId, setUserId] = useState('');
+	const [showModal, setShowModal] = useState(false);
+	const [formData, setFormData] = useState({
+		paymentAmount: '',
+	});
 
-  const data = useSelector((state: any) => state.user.userdata || []);
+	const data = useSelector((state: any) => state.user.userdata || []);
 
-  useEffect(() => {
-    dispatch(GetAllUser());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(GetAllUser());
+	}, [dispatch]);
 
-  // Columns definition
-  const columns: any = [
+	// Columns definition
+	const columns: any = [
 		{
 			name: 'Name',
 			id: 'Name',
@@ -48,50 +57,45 @@ const User = () => {
 			id: 'action',
 			sortable: true,
 			width: '100px',
-		
 		},
 	];
 
-  // Filter the data based on search input
-  const filteredData =[]
+	// Filter the data based on search input
+	const filteredData: any = [];
 
-  console.log(data);
+	console.log(data);
 
+	return (
+		<>
+			<Card>
+				<Card.Header>
+					<h4 className='card-title mb-0'>All Users</h4>
+				</Card.Header>
 
-  return (
-    <>
-      <Card>
-        <Card.Header>
-          <h4 className="card-title mb-0">All Users</h4>
-        </Card.Header>
+				<Card.Body>
+					<Container>
+						<FormControl
+							type='text'
+							placeholder='Search users...'
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+							className='mb-3'
+						/>
 
-        <Card.Body>
-				<Container>
-        
-          <FormControl
-            type="text"
-            placeholder="Search users..."
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-            className="mb-3"
-          />
-        
 						<DataTable
 							columns={columns}
 							data={filteredData.length === 0 ? data.user : filteredData}
 							pagination
 							defaultSortFieldId={1}
 							subHeader
-							
 							persistTableHead
 							theme={'default'}
 						/>
-				</Container>
-        </Card.Body>
-      </Card>
-  
-    </>
-  );
+					</Container>
+				</Card.Body>
+			</Card>
+		</>
+	);
 };
 
 export default User;
