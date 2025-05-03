d029c429d266aaa97ffcbe98e8a0fde545fad1fa
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-03 18:39:45 +0530

    added changes

--- pages/client/add-client.tsx
+++ pages/client/add-client.tsx
@@ -1,193 +1,208 @@
-import React, { ReactElement, useEffect } from "react";
-import Head from "next/head";
-import Layout from "@common/Layout";
-import Breadcrumb from "@common/Breadcrumb";
-import FormLabel from "@common/FormLabel";
-import FormInput from "@common/FormInput";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import { fetchOrganisation } from "Components/slices/organisation/thunk";
-import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
-import { AddNewClient } from "Components/slices/client/thunk";
+import React, { ReactElement, useEffect } from 'react';
+import Head from 'next/head';
+import Layout from '@common/Layout';
+import Breadcrumb from '@common/Breadcrumb';
+import FormLabel from '@common/FormLabel';
+import FormInput from '@common/FormInput';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { AddNewClient } from 'Components/slices/client/thunk';
 
 const AddClient = () => {
-  const router = useRouter();
-  const dispatch: any = useDispatch();
-  const { organisationdata, isLoading } = useSelector((state: any) => ({
-    organisationdata: state.organisationdata.organisationdata,
-    isLoading: state.client.isLoading,
-  }));
+	const router = useRouter();
+	const dispatch: any = useDispatch();
+	const { isLoading } = useSelector((state: any) => ({
+		isLoading: state.client.isLoading,
+	}));
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      address: "",
-      phone: "",
-      contactPerson: "",
-      parentOrganization: "",
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required("Required"),
-      email: Yup.string().email("Invalid email address").required("Required"),
-      address: Yup.string().required("Required"),
-      phone: Yup.string()
-        .matches(/^\d+$/, "Please enter only numbers")
-        .required("Contact-Number is required")
-        .min(10, "Contact Number should not be long less than 10 digits")
-        .max(10, "Contact Number should not be long more than 10 digits"),
-      contactPerson: Yup.string().required("Required"),
-      parentOrganization: Yup.string().required("Required"),
-    }),
-    onSubmit: (values) => {
-      formik.resetForm();
-      dispatch(AddNewClient(values, router));
-    },
-  });
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			email: '',
+			address: '',
+			phone: '',
+			contactPerson: '',
+			parentOrganization: '',
+		},
+		validationSchema: Yup.object({
+			name: Yup.string().required('Required'),
+			email: Yup.string().email('Invalid email address').required('Required'),
+			address: Yup.string().required('Required'),
+			phone: Yup.string()
+				.matches(/^\d+$/, 'Please enter only numbers')
+				.required('Contact-Number is required')
+				.min(10, 'Contact Number should not be long less than 10 digits')
+				.max(10, 'Contact Number should not be long more than 10 digits'),
+			contactPerson: Yup.string().required('Required'),
+			parentOrganization: Yup.string().required('Required'),
+		}),
+		onSubmit: (values) => {
+			formik.resetForm();
+			dispatch(AddNewClient(values, router));
+		},
+	});
 
-  useEffect(() => {
-    dispatch(fetchOrganisation());
-  }, []);
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Add Client | Midas - HRMS</title>
+			</Head>
 
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Add Client | Midas - HRMS</title>
-      </Head>
+			<div className='page-content'>
+				<Breadcrumb
+					breadcrumbItem='Add Client'
+					breadcrumb='Dashboard'
+				/>
+				<Container fluid={true}>
+					<form onSubmit={formik.handleSubmit}>
+						<Row className='mt-n1'>
+							<Col
+								lg={6}
+								xs={6}>
+								<FormLabel
+									for='name'
+									labelname='Name'
+								/>
+								<FormInput
+									inpType='text'
+									inpchange={formik.handleChange}
+									inpId='name'
+									inpblur={formik.handleBlur}
+									inpvalue={formik.values.name}
+									inpPlaceholder='Enter name'
+								/>
+								<span className='text-danger'>
+									{formik.touched.name && formik.errors.name ? (
+										<div className='text-danger'>{formik.errors.name}</div>
+									) : null}
+								</span>
+							</Col>
+							<Col
+								lg={6}
+								xs={6}>
+								<FormLabel
+									for='email'
+									labelname='Email'
+								/>
+								<FormInput
+									inpType='email'
+									inpId='email'
+									inpchange={formik.handleChange}
+									inpblur={formik.handleBlur}
+									inpvalue={formik.values.email}
+									inpPlaceholder='Enter email address'
+								/>
+								<span className='text-danger'>
+									{formik.touched.email && formik.errors.email ? (
+										<div className='text-danger'>{formik.errors.email}</div>
+									) : null}
+								</span>
+							</Col>
 
-      <div className="page-content">
-        <Breadcrumb breadcrumbItem="Add Client" breadcrumb="Dashboard" />
-        <Container fluid={true}>
-          <form onSubmit={formik.handleSubmit}>
-            <Row className="mt-n1">
-              <Col lg={6} xs={6}>
-                <FormLabel for="name" labelname="Name" />
-                <FormInput
-                  inpType="text"
-                  inpchange={formik.handleChange}
-                  inpId="name"
-                  inpblur={formik.handleBlur}
-                  inpvalue={formik.values.name}
-                  inpPlaceholder="Enter name"
-                />
-                <span className="text-danger">
-                  {formik.touched.name && formik.errors.name ? (
-                    <div className="text-danger">{formik.errors.name}</div>
-                  ) : null}
-                </span>
-              </Col>
-              <Col lg={6} xs={6}>
-                <FormLabel for="email" labelname="Email" />
-                <FormInput
-                  inpType="email"
-                  inpId="email"
-                  inpchange={formik.handleChange}
-                  inpblur={formik.handleBlur}
-                  inpvalue={formik.values.email}
-                  inpPlaceholder="Enter email address"
-                />
-                <span className="text-danger">
-                  {formik.touched.email && formik.errors.email ? (
-                    <div className="text-danger">{formik.errors.email}</div>
-                  ) : null}
-                </span>
-              </Col>
+							<Col
+								className='mt-3'
+								lg={6}
+								xs={6}>
+								<FormLabel
+									for='address'
+									labelname='Address'
+								/>
+								<FormInput
+									inpType='text'
+									inpId='address'
+									inpchange={formik.handleChange}
+									inpblur={formik.handleBlur}
+									inpvalue={formik.values.address}
+									inpPlaceholder='Enter Address'
+								/>
+								<span className='text-danger'>
+									{formik.touched.address && formik.errors.address ? (
+										<div className='text-danger'>{formik.errors.address}</div>
+									) : null}
+								</span>
+							</Col>
+							<Col
+								className='mt-3'
+								lg={6}
+								xs={6}>
+								<FormLabel
+									for='phone'
+									labelname='Phone No.'
+								/>
+								<FormInput
+									inpType='number'
+									inpId='phone'
+									inpchange={formik.handleChange}
+									inpblur={formik.handleBlur}
+									inpvalue={formik.values.phone}
+									inpPlaceholder='Enter Phone Number'
+								/>
+								<span className='text-danger'>
+									{formik.touched.phone && formik.errors.phone ? (
+										<div className='text-danger'>{formik.errors.phone}</div>
+									) : null}
+								</span>
+							</Col>
 
-              <Col className="mt-3" lg={6} xs={6}>
-                <FormLabel for="address" labelname="Address" />
-                <FormInput
-                  inpType="text"
-                  inpId="address"
-                  inpchange={formik.handleChange}
-                  inpblur={formik.handleBlur}
-                  inpvalue={formik.values.address}
-                  inpPlaceholder="Enter Address"
-                />
-                <span className="text-danger">
-                  {formik.touched.address && formik.errors.address ? (
-                    <div className="text-danger">{formik.errors.address}</div>
-                  ) : null}
-                </span>
-              </Col>
-              <Col className="mt-3" lg={6} xs={6}>
-                <FormLabel for="phone" labelname="Phone No." />
-                <FormInput
-                  inpType="number"
-                  inpId="phone"
-                  inpchange={formik.handleChange}
-                  inpblur={formik.handleBlur}
-                  inpvalue={formik.values.phone}
-                  inpPlaceholder="Enter Phone Number"
-                />
-                <span className="text-danger">
-                  {formik.touched.phone && formik.errors.phone ? (
-                    <div className="text-danger">{formik.errors.phone}</div>
-                  ) : null}
-                </span>
-              </Col>
+							<Col
+								className='mt-3'
+								lg={6}
+								xs={6}>
+								<FormLabel
+									for='contactPerson'
+									labelname='Contact Person Name'
+								/>
+								<FormInput
+									inpType='text'
+									inpId='contactPerson'
+									inpchange={formik.handleChange}
+									inpblur={formik.handleBlur}
+									inpvalue={formik.values.contactPerson}
+									inpPlaceholder='Enter Contact Person Name'
+								/>
+								<span className='text-danger'>
+									{formik.touched.contactPerson &&
+									formik.errors.contactPerson ? (
+										<div className='text-danger'>
+											{formik.errors.contactPerson}
+										</div>
+									) : null}
+								</span>
+							</Col>
 
-              <Col className="mt-3" lg={6} xs={6}>
-                <FormLabel
-                  for="contactPerson"
-                  labelname="Contact Person Name"
-                />
-                <FormInput
-                  inpType="text"
-                  inpId="contactPerson"
-                  inpchange={formik.handleChange}
-                  inpblur={formik.handleBlur}
-                  inpvalue={formik.values.contactPerson}
-                  inpPlaceholder="Enter Contact Person Name"
-                />
-                <span className="text-danger">
-                  {formik.touched.contactPerson &&
-                  formik.errors.contactPerson ? (
-                    <div className="text-danger">
-                      {formik.errors.contactPerson}
-                    </div>
-                  ) : null}
-                </span>
-              </Col>
+							<Col
+								className='mt-3'
+								lg={6}
+								xs={6}>
+								<FormLabel
+									for='organisationName'
+									labelname='Parent Organization'
+								/>
+							</Col>
 
-              <Col className="mt-3" lg={6} xs={6}>
-                <FormLabel
-                  for="organisationName"
-                  labelname="Parent Organization"
-                />
-
-                <select
-                  className="form-select"
-                  aria-label="Default select example"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  name="parentOrganization"
-                  value={formik.values.parentOrganization}
-                >
-                  <option selected>Open this select menu</option>
-
-                  {organisationdata.map((item: any) => {
-                    return <option value={item.id}>{item.name}</option>;
-                  })}
-                </select>
-              </Col>
-
-              <Col lg={12} className="mt-4">
-                <Button variant="primary" type="submit" disabled={isLoading}>
-                  Save
-                </Button>
-              </Col>
-            </Row>
-          </form>
-        </Container>
-      </div>
-    </React.Fragment>
-  );
+							<Col
+								lg={12}
+								className='mt-4'>
+								<Button
+									variant='primary'
+									type='submit'
+									disabled={isLoading}>
+									Save
+								</Button>
+							</Col>
+						</Row>
+					</form>
+				</Container>
+			</div>
+		</React.Fragment>
+	);
 };
 
 AddClient.getLayout = (page: ReactElement) => {
-  return <Layout>{page}</Layout>;
+	return <Layout>{page}</Layout>;
 };
 
 export default AddClient;
